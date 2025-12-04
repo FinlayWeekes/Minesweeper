7834da57f0dea10f8da44b43db4e2303ab822db9
FinlayWeekes <u241340@crgs.org.uk>
2025-12-04 10:02:47 +0000

    Add files via upload

---
+++ Minesweeper-main/Minesweeper/Board.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Minesweeper
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resetButton = new System.Windows.Forms.Button();
+            this.MineCountDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.resetButton.Location = new System.Drawing.Point(153, 0);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(80, 40);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // MineCountDisplay
+            // 
+            this.MineCountDisplay.AutoSize = true;
+            this.MineCountDisplay.Location = new System.Drawing.Point(45, 13);
+            this.MineCountDisplay.Name = "MineCountDisplay";
+            this.MineCountDisplay.Size = new System.Drawing.Size(35, 13);
+            this.MineCountDisplay.TabIndex = 1;
+            this.MineCountDisplay.Text = "label1";
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 401);
+            this.Controls.Add(this.MineCountDisplay);
+            this.Controls.Add(this.resetButton);
+            this.Name = "Board";
+            this.Text = "Board";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label MineCountDisplay;
+    }
+}
---
+++ Minesweeper-main/Minesweeper/Board.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Board : Form
+    {
+        private DisplayCell[,] cells;
+        private bool firstClick = true;
+        private const int headerSize = 40;
+        private int remaningHiddenCells;
+        private int remaningMines;
+        private int cellSize;
+        private int height;
+        private int width;
+        private int mineCount;
+        private int clickCount;
+
+        private Color hiddenCol = Color.White;
+        private Color revealedCol = Color.LightGray;
+        private Color explodedMineCol = Color.Red;
+        private Color revealedMineCol = Color.DarkRed;
+
+
+        public Board(int Height, int Width, int Minecount)
+        {
+            InitializeComponent();
+
+            clickCount = 0;
+            height = Height;
+            width = Width;
+            mineCount = Minecount;
+            remaningHiddenCells = width*height;
+            remaningMines = mineCount;
+            MineCountDisplay.Text = remaningMines.ToString();
+
+            int maxSize = 40;
+            cellSize = CalculateCellSize(maxSize);
+
+            this.ClientSize = new Size(width * cellSize, height * cellSize + headerSize);
+
+            PopulateCells();
+        }
+
+        private void PopulateCells()
+        {
+            cells = new DisplayCell[height, width];
+
+            SuspendLayout();
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    Button btn = new Button();
+                    btn.Width = cellSize;
+                    btn.Height = cellSize;
+                    btn.Location = new Point(col * cellSize, row * cellSize + headerSize);
+                    btn.Name = "Cell" + (row * width + col);
+                    btn.Size = new Size(cellSize, cellSize);
+                    btn.BackColor = hiddenCol;
+
+                    btn.Tag = new Point(col, row);
+
+                    btn.MouseUp += CellClick;
+
+                    Controls.Add(btn);
+                    cells[row, col] = new DisplayCell(btn);
+                }
+            }
+
+            ResumeLayout(false);
+        }
+        private void PopulateMines(int firstCol, int firstRow)
+        {
+            Random rnd = new Random();
+
+            // places mines where there are no overlapping and to make te first cell clear
+            for (int i = 0; i < mineCount; i++)
+            {
+                int col;
+                int row;
+                bool mineAdded;
+
+                do
+                {
+                    mineAdded = false;
+                    col = rnd.Next(0, width);
+                    row = rnd.Next(0, height);
+
+                    bool nextToStart = false;
+
+                    for (int xOfset = -1; xOfset <= 1; xOfset++)
+                    {
+                        for (int yOfset = -1; yOfset <= 1; yOfset++)
+                        {
+                            if (col + xOfset >= 0 &&
+                                col + xOfset < cells.GetLength(1) &&
+                                row + yOfset >= 0 &&
+                                row + yOfset < cells.GetLength(0) &&
+                                firstCol + xOfset == col && 
+                                firstRow + yOfset == row)
+                            {
+                                nextToStart = true;
+                            }
+                        }
+                    }
+
+                    if (!nextToStart && !cells[row, col].IsMine)
+                    {
+                        mineAdded = true;
+                        cells[row, col].SetMine();
+
+                        for (int xOfset = -1; xOfset <= 1; xOfset++)
+                        {
+                            for (int yOfset = -1; yOfset <= 1; yOfset++)
+                            {
+                                if (col + xOfset >= 0 && 
+                                    col + xOfset < cells.GetLength(1) && 
+                                    row + yOfset >= 0 && 
+                                    row + yOfset < cells.GetLength(0))
+                                {
+                                    cells[row + yOfset, col + xOfset].IncreaseValue();
+                                }
+                            }
+                        }
+                    }
+                }
+                while (!mineAdded);
+            }
+
+
+        }
+        private void CellClick(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (GetDisplayCell(btn).IsHidden)
+                {
+                    FlagCell(btn);
+                }
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                if (GetDisplayCell(btn).IsHidden)
+                {
+                    RevealCell(btn);
+                }
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                if (!GetDisplayCell(btn).IsHidden)
+                {
+                    Chord(btn);
+                }
+            }
+
+            clickCount++;
+        }
+        private void Chord(Button btn)
+        {
+            int adjacentHiddenCells = 0;
+            int adjacentFlags = 0;
+            for (int xOfset = -1; xOfset <= 1; xOfset++)
+            {
+                for (int yOfset = -1; yOfset <= 1; yOfset++)
+                {
+                    if (((Point)btn.Tag).X + xOfset >= 0 && ((Point)btn.Tag).X + xOfset < cells.GetLength(1) && ((Point)btn.Tag).Y + yOfset >= 0 && ((Point)btn.Tag).Y + yOfset < cells.GetLength(0))
+                    {
+                        if (cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsHidden)
+                        {
+                            adjacentHiddenCells++;
+                            if (cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsFlagged)
+                            {
+                                adjacentFlags++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // opens all adjacent cells if the cells effective value = 0
+            if (GetDisplayCell(btn).Value == adjacentFlags)
+            {
+                for (int xOfset = -1; xOfset <= 1; xOfset++)
+                {
+                    for (int yOfset = -1; yOfset <= 1; yOfset++)
+                    {
+                        if (((Point)btn.Tag).X + xOfset >= 0 &&
+                            ((Point)btn.Tag).X + xOfset < cells.GetLength(1) &&
+                            ((Point)btn.Tag).Y + yOfset >= 0 && 
+                            ((Point)btn.Tag).Y + yOfset < cells.GetLength(0) &&
+                            !cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsFlagged &&
+                            cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsHidden)
+                        {
+                            RevealCell(cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].Btn);
+                        }
+                    }
+                }
+            }
+            // flags all adjacent cells if the cells value is the number of adjacent cells
+            else if (GetDisplayCell(btn).Value == adjacentHiddenCells)
+            {
+                for (int xOfset = -1; xOfset <= 1; xOfset++)
+                {
+                    for (int yOfset = -1; yOfset <= 1; yOfset++)
+                    {
+                        if (((Point)btn.Tag).X + xOfset >= 0 && 
+                            ((Point)btn.Tag).X + xOfset < cells.GetLength(1) &&
+                            ((Point)btn.Tag).Y + yOfset >= 0 && 
+                            ((Point)btn.Tag).Y + yOfset < cells.GetLength(0) &&
+                            !cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsFlagged &&
+                            cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsHidden)
+                        {
+                            FlagCell(cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].Btn);
+                        }
+                    }
+                }
+            }
+        }
+        private void FlagCell(Button btn)
+        {
+            if (!GetDisplayCell(btn).IsHidden) return;
+
+            GetDisplayCell(btn).Flag();
+
+            if (btn.Text == "F")
+            {
+                btn.Text = "";
+                remaningMines++;
+                MineCountDisplay.Text = remaningMines.ToString();
+            }
+            else
+            {
+                btn.Text = "F";
+                remaningMines--;
+                MineCountDisplay.Text = remaningMines.ToString();
+            }
+        }
+        private void RevealCell(Button btn)
+        {
+            // disables the cell from interactions
+            // makes the background grey if no mine
+            // makes the background red if mine and runs RevealMine()
+            remaningHiddenCells--;
+
+            if (firstClick)
+            {
+                firstClick = false;
+                PopulateMines(((Point)btn.Tag).X, ((Point)btn.Tag).Y);
+            }
+
+            if (GetDisplayCell(btn).IsMine)
+            {
+                btn.BackColor = explodedMineCol;
+                btn.Text = "";
+                RevealMine();
+                return;
+            }
+
+            btn.BackColor = revealedCol;
+            GetDisplayCell(btn).Open();
+
+            // opens all ajacent is a clear cell is revealed
+            if (GetDisplayCell(btn).Value == 0)
+            {
+                btn.Text = "";
+
+                for (int xOfset = -1; xOfset <= 1; xOfset++)
+                {
+                    for (int yOfset = -1; yOfset <= 1; yOfset++)
+                    {
+                        if (((Point)btn.Tag).X + xOfset >= 0 && 
+                            ((Point)btn.Tag).X + xOfset < cells.GetLength(1) && 
+                            ((Point)btn.Tag).Y + yOfset >= 0 && 
+                            ((Point)btn.Tag).Y + yOfset < cells.GetLength(0) &&
+                            cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].IsHidden)
+                        {
+                            RevealCell(cells[((Point)btn.Tag).Y + yOfset, ((Point)btn.Tag).X + xOfset].Btn);
+                        }
+                        
+                    }
+                }
+
+            }
+            else
+            {
+                btn.Text = GetDisplayCell(btn).Value.ToString();
+                AddColourToText(btn.Text, btn);
+            }
+
+            if (HasWon())
+            {
+                resetButton.Text = "YOU WIN";
+                foreach (DisplayCell cell in cells)
+                {
+                    cell.Btn.Enabled = false;
+                }
+            }
+        }
+        private DisplayCell GetDisplayCell(Button btn)
+        {
+            return cells[((Point)btn.Tag).Y, ((Point)btn.Tag).X];
+        }
+        private bool HasWon()
+        {
+            // checks if the player has won
+            if (remaningHiddenCells == mineCount)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void RevealMine()
+        {
+            // reveals all mines on the board and disables all cells
+
+            foreach (DisplayCell cell in cells)
+            {
+                if (cell.IsMine && (cell.Btn.BackColor != explodedMineCol || explodedMineCol == revealedMineCol))
+                {
+                    cell.Btn.BackColor = revealedMineCol;
+                }
+                cell.Btn.Enabled = false;
+            }
+        }
+        private int CalculateCellSize(int maxSize)
+        {
+            Rectangle workingArea;
+            workingArea = Screen.PrimaryScreen.WorkingArea;
+
+            int maxCellWidth = workingArea.Width / width;
+            int maxCellHeight = (workingArea.Height-headerSize) / height;
+            int maxCell = Math.Min(maxCellWidth, maxCellHeight);
+
+            return Math.Min(maxSize, maxCell);
+        }
+        public void AddColourToText(string value, Button btn)
+        {
+            switch (value)
+            {
+                case "1": btn.ForeColor = Color.DarkBlue; break;
+                case "2": btn.ForeColor = Color.Green; break;
+                case "3": btn.ForeColor = Color.Red; break;
+                case "4": btn.ForeColor = Color.Navy; break;
+                case "5": btn.ForeColor = Color.DarkRed; break;
+                case "6": btn.ForeColor = Color.Cyan; break;
+                case "7": btn.ForeColor = Color.Black; break;
+                case "8": btn.ForeColor = Color.DarkGray; break;
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            firstClick = true;
+            remaningMines = mineCount;
+            clickCount = 0;
+            remaningHiddenCells = width * height;
+            resetButton.Text = "Reset";
+
+            foreach (DisplayCell cell in cells)
+            {
+                cell.ResetValues();
+                cell.Btn.BackColor = hiddenCol;
+            }
+        }
+    }
+}
