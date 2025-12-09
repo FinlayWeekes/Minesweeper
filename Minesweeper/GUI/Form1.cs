456b5adb4d9fdafc8bad206193a62f7d2fa80caf
FinlayWeekes <u241340@crgs.org.uk>
2025-12-09 14:29:50 +0000

    Add files via upload

---
+++ Minesweeper/GUI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OpenBoard(int width, int height, int mineCount, string difficulty)
+        {
+            Board board = new Board(width, height, mineCount, difficulty);
+            board.Show();
+            this.Hide();
+        }
+
+        private void customButton_Click(object sender, EventArgs e)
+        {
+            CustomForm customForm = new CustomForm();
+            customForm.Show();
+            this.Hide();
+        }
+
+        private void beginnerButton_Click(object sender, EventArgs e)
+        {
+            OpenBoard(10, 10, 10, "Beginner");
+        }
+
+        private void amateurButton_Click(object sender, EventArgs e)
+        {
+            OpenBoard(12, 12, 20, "Amateur");
+        }
+        private void intermediateButton_Click(object sender, EventArgs e)
+        {
+            OpenBoard(20, 20, 40, "Intermediate");
+        }
+
+        private void expertButton_Click(object sender, EventArgs e)
+        {
+            OpenBoard(30, 16, 99, "Expert");
+        }
+
+        private void buttonMaster_Click(object sender, EventArgs e)
+        {
+            OpenBoard(40, 20, 200, "Master");
+        }
+    }
+}
---
+++ Minesweeper/GUI/LogicCell.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+
+class LogicCell : Cell
+{
+    private bool canBeChecked;
+    public bool CanBeChecked
+    {
+        get
+        {
+            return canBeChecked;
+        }
+    }
+    private List<LogicCell> adjacentCells;
+    public List<LogicCell> AdjacentCells
+    {
+        get
+        {
+            return adjacentCells;
+        }
+    }
+    private int effectiveValue;
+    public int EffectiveValue
+    {
+        get
+        {
+            return effectiveValue;
+        }
+    }
+    private int id;
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+    }
+    private int adjacentHiddenCellsCount;
+    public int AdjacentHiddenCellsCount
+    {
+        get
+        {
+            return adjacentHiddenCellsCount;
+        }
+    }
+    public LogicCell(int x, int y, bool isHidden, int boardWidth, int boardHeight, int id)
+    : base()
+    {
+        adjacentCells = new List<LogicCell>();
+        adjacentHiddenCellsCount = 0;
+        this.id = id;
+    }
+    public LogicCell(LogicCell cell, bool addAdjacent)
+    {
+        adjacentCells = new List<LogicCell>();
+
+        if (addAdjacent)
+        {
+            foreach (LogicCell adjacentCell in cell.AdjacentCells)
+            {
+                this.AdjacentCells.Add(new LogicCell(adjacentCell, false));
+            }
+        }
+
+        this.effectiveValue = cell.EffectiveValue;
+        this.isHidden = cell.IsHidden;
+        this.isFlagged = cell.IsFlagged;
+        this.id = cell.Id;
+        this.adjacentHiddenCellsCount = cell.AdjacentHiddenCellsCount;
+    }
+    public void SetValues()
+    {
+        effectiveValue = value;
+
+        canBeChecked = !isHidden;
+        if (canBeChecked) canBeChecked = value != 0;
+    }
+    public void AddAdjacentCell(LogicCell cell)
+    {
+        if (cell.IsMine && !this.isMine) this.value++;
+
+        adjacentCells.Add(cell);
+        if (cell.IsHidden)
+        {
+            adjacentHiddenCellsCount++;
+        }
+    }
+    public override void Flag()
+    {
+        isFlagged = true;
+        Complete();
+
+        foreach (LogicCell cell in adjacentCells)
+        {
+            if (!cell.IsMine)
+            {
+                cell.LowerEffectiveValue();
+            }
+        }
+    }
+    public void SetFlagTrue()
+    {
+        isFlagged = true;
+    }
+    public void LowerEfectiveValue()
+    {
+        effectiveValue--;
+    }
+    private void LowerEffectiveValue()
+    {
+        effectiveValue--;
+    }
+    public override bool Open()
+    {
+        if (isMine)
+        {
+            Console.WriteLine("clicked on mine");
+            Console.ReadLine();
+        }
+
+        isHidden = false;
+        canBeChecked = value != 0;
+        foreach (LogicCell cell in adjacentCells)
+        {
+            cell.LowerAdjacentHiddenCellsCount();
+        }
+
+        if (value > 0) return false;
+        if (isMine) return true;
+
+        foreach (LogicCell cell in adjacentCells)
+        {
+            if (cell.isHidden) cell.Open();
+        }
+
+        return false;
+    }
+    public void LowerAdjacentHiddenCellsCount()
+    {
+        adjacentHiddenCellsCount--;
+    }
+    public void Complete()
+    {
+        canBeChecked = false;
+    }
+    public override void SetMine()
+    {
+        isMine = true;
+
+        foreach (LogicCell cell in adjacentCells)
+        {
+            if (!cell.IsMine) cell.value++;
+        }
+    }
+}
