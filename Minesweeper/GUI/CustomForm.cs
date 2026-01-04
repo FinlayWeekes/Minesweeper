2f7a248d8ba662243e0e76618168f529810e0614
FinlayWeekes <u241340@crgs.org.uk>
2026-01-04 19:41:24 +0000

    Add files via upload

---
+++ Minesweeper/GUI/CustomForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Minesweeper.AI;
+
+namespace Minesweeper.GUI
+{
+    public partial class CustomForm : Form
+    {
+        private bool lightMode;
+        private string name;
+        public CustomForm(bool LightMode, string Name)
+        {
+            InitializeComponent();
+
+            name = Name;
+            LightMode = lightMode;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int height = int.Parse(this.heightTextBox.Text);
+            int width = int.Parse(this.widthTextBox.Text);
+            int mineCount = int.Parse(this.mineCountTextBox.Text);
+
+            Board board = new Board(new Difficulty(width, height, mineCount, null), lightMode, name);
+            board.Show();
+            this.Hide();
+        }
+    }
+}
