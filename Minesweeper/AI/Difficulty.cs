ac9a21b90ead9530e993de0b12b70241fc12d1cf
FinlayWeekes <u241340@crgs.org.uk>
2026-01-14 21:09:34 +0000

    Add files via upload

---
+++ Minesweeper/AI/Difficulty.cs
@@ -0,0 +1,210 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Minesweeper.AI
+{
+    public class Difficulty
+    {
+        public List<Func<LogicCell, Grid, int, int, bool>> EasyPatterns
+        {
+            get
+            {
+                return easyPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> easyPatterns;
+        public List<Func<LogicCell, Grid, int, int, bool>> OptionalPatterns
+        {
+            get
+            {
+                return optionalPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> optionalPatterns;
+        public List<Func<LogicCell, Grid, int, int, bool>> EssentialPatterns
+        {
+            get
+            {
+                return essentialPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> essentialPatterns;
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+        }
+        private int width;
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+        private int height;
+        public int MineCount
+        {
+            get
+            {
+                return mineCount;
+            }
+        }
+        private int mineCount;
+        public string Name
+        {
+            get { return name; }
+        }
+        private string name;
+        public bool Custom
+        {
+            get
+            {
+                return custom;
+            }
+        }
+        private bool custom;
+        public bool Random
+        {
+            get { return random; }
+        }
+        private bool random;
+        
+        private Dictionary<string, Func<LogicCell, Grid, int, int, bool>> patternsDict = new Dictionary<string, Func<LogicCell, Grid, int, int, bool>>
+        {
+            // Beginner
+            { "B1", BeginnerAI.PB1 },
+            { "B2", BeginnerAI.PB2 },
+
+            // Amateur
+            { "1-1C", AmateurAI.P1_1C },
+            { "2-1", AmateurAI.P2_1 },
+
+            // Intermediate
+            { "1-1CR", IntermediateAI.P1_1CR },
+            { "2-1C", IntermediateAI.P2_1C },
+            { "2-1R", IntermediateAI.P2_1R },
+            { "2-1CR", IntermediateAI.P2_1CR },
+            { "H1", IntermediateAI.PH1 },
+            { "H2", IntermediateAI.PH2 },
+            { "H3", IntermediateAI.PH3 },
+
+            // Expert
+            { "1-1CR+", ExpertAI.P1_1CRx },
+            { "2-1CR+", ExpertAI.P2_1CRx },
+            { "2-1R+", ExpertAI.P1_1CRx },
+            { "H1R", ExpertAI.PH1R },
+            { "H2R", ExpertAI.PH2R },
+            { "H3R", ExpertAI.PH3R },
+            { "T1", ExpertAI.PT1 },
+            { "T2", ExpertAI.PT2 },
+            { "T3", ExpertAI.PT3 },
+            { "T4", ExpertAI.PT4 },
+
+            // Master
+            { "T1R", MasterAI.PT1R },
+            { "T2R", MasterAI.PT2R },
+            { "T3R", MasterAI.PT3R },
+            { "T4R", MasterAI.PT4R },
+            { "1-3-VC", MasterAI.P1_3_1VC },
+            { "1-3-1VCR", MasterAI.P1_3_1VCR },
+            { "2-2-2VC", MasterAI.P2_2_2VC },
+            { "2-2-2VCR", MasterAI.P2_2_2VCR },
+            { "T-Pattern", MasterAI.PT_Pattern},
+            { "MineCounting", MasterAI.PMineCounting },
+
+            // Other
+            { "H1-H3", OtherAI.PH1_H3 },
+        };
+
+        public Difficulty(string difficultyName)
+        {
+            string[] easyNames;
+            string[] optionalNames;
+            string[] essentialNames;
+
+            using (StreamReader sr = new StreamReader(GetTextFileName(difficultyName)))
+            {
+                sr.ReadLine();
+                this.name = sr.ReadLine();
+
+                sr.ReadLine();
+                this.width = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.height = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.mineCount = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.custom = Convert.ToBoolean(sr.ReadLine());
+
+                sr.ReadLine();
+                easyNames = sr.ReadLine().Split(',');
+
+                sr.ReadLine();
+                optionalNames = sr.ReadLine().Split(',');
+
+                sr.ReadLine();
+                essentialNames = sr.ReadLine().Split(',');
+            }
+
+            this.easyPatterns = GetPatterns(easyNames);
+            this.optionalPatterns = GetPatterns(optionalNames);
+            this.essentialPatterns = GetPatterns(essentialNames);
+
+            CheckForRandom();
+        }
+        public Difficulty(int width, int height, int mineCount, List<string> easyPatternNames, List<string> optionalPatternNames, List<string> essentialPatternNames)
+        {
+            this.easyPatterns = GetPatterns(ListToArray(easyPatternNames));
+            this.optionalPatterns = GetPatterns(ListToArray(optionalPatternNames));
+            this.essentialPatterns = GetPatterns(ListToArray(essentialPatternNames));
+            this.width = width;
+            this.height = height;
+            this.mineCount = mineCount;
+            this.name = "Custom";
+            this.custom = true;
+
+            CheckForRandom();
+        }
+
+        private void CheckForRandom()
+        {
+            random = easyPatterns.Count == 0 && optionalPatterns.Count == 0 && essentialPatterns.Count == 0; 
+        }
+        private string GetTextFileName(string difficultyName)
+        {
+            return "Difficulties/" + difficultyName.ToLower() + "Difficulty.txt";
+        }
+        private string[] ListToArray(List<string> list)
+        {
+            string[] array = new string[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array [i] = list[i];
+            }
+            return array;
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> GetPatterns(string[] patternNames)
+        {
+            List<Func<LogicCell, Grid, int, int, bool>> patterns = new List<Func<LogicCell, Grid, int, int, bool>>();
+
+            if (patternNames.Length == 0 || patternNames[0] == "null") return patterns;
+
+            foreach (string pattern in patternNames)
+            {
+                patterns.Add(patternsDict[pattern]);
+            }
+
+            return patterns;
+        }
+    }
+}
---
+++ Minesweeper/GUI/Leaderboards.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper.GUI
+{
+    public partial class Leaderboards : Form
+    {
+        private int topTimesCount;
+        private int[] betchelsMin;
+
+        public Leaderboards(int topTimesCount, int[] betchelsMin)
+        {
+            this.topTimesCount = topTimesCount;
+            this.betchelsMin = betchelsMin;
+
+            InitializeComponent();
+
+            string[] difNames = { "Beginner", "Amateur", "Intermediate", "Expert", "Master" };
+
+            foreach (string difName in difNames)
+            {
+                List<string> times = GetTimes(difName);
+
+                System.Windows.Forms.Label label = GetTimesLabel(difName);
+
+                string betchels = Get3BVMin(difName);
+
+                AddTimesToLabel(label, times, betchels);
+            }
+        }
+
+        private List<string> GetTimes(string difName)
+        {
+            List<string> times = new List<string>();
+
+            using (StreamReader sr = new StreamReader(TimesTextName(difName)))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    times.Add(line);
+                }
+            }
+
+            return times;
+        }
+        private System.Windows.Forms.Label GetTimesLabel(string dif)
+        {
+            switch (dif)
+            {
+                case "Beginner":
+                    {
+                        return beginnerTimes;
+                    }
+                case "Amateur":
+                    {
+                        return amateurTimes;
+                    }
+                case "Intermediate":
+                    {
+                        return intermediateTimes;
+                    }
+                case "Expert":
+                    {
+                        return expertTimes;
+                    }
+                default:
+                    {
+                        return masterTimes;
+                    }
+            }
+        }
+        private string Get3BVMin(string dif)
+        {
+            switch (dif)
+            {
+                case "Beginner":
+                    {
+                        return Convert.ToString(betchelsMin[0]);
+                    }
+                case "Amateur":
+                    {
+                        return Convert.ToString(betchelsMin[1]);
+                    }
+                case "Intermediate":
+                    {
+                        return Convert.ToString(betchelsMin[2]);
+                    }
+                case "Expert":
+                    {
+                        return Convert.ToString(betchelsMin[3]);
+                    }
+                default:
+                    {
+                        return Convert.ToString(betchelsMin[4]);
+                    }
+            }
+        }
+        private string TimesTextName(string difName)
+        {
+            return "Times/" + difName.ToLower() + "Times.txt";
+        }
+
+
+        private void AddTimesToLabel(System.Windows.Forms.Label label, List<string> times, string betchels)
+        {
+            // times are in the form: {name}-{time}
+            label.Text = "";
+
+            for (int i = 0; i < times.Count; i++)
+            {
+                label.Text += Convert.ToString(i + 1) + ". " + GetNameFromTime(times[i]) + "- " + FormatTime(times[i]) + "\n";
+            }
+
+            for (int i = 0; i < topTimesCount - times.Count; i++)
+            {
+                label.Text += "\n";
+            }
+
+            label.Text += "\n3BV Minimum: " + betchels;
+        }
+        private string GetNameFromTime(string record)
+        {
+            string[] splitRecord = record.Split('-');
+            string name = splitRecord[0];
+            
+            // itterates incase the user has a "-" in their name which is the character used to sepparate the name and time in the text file
+            for (int i = 1; i < splitRecord.Length-1; i++)
+            {
+                name += "-" + splitRecord[i];
+            }
+
+            return name;
+        }
+        private string FormatTime(string stringTime)
+        {
+            // returns the time in the form: MM:SS.D or SS.D or S.D 
+
+            int time = GetTimeFromRecord(stringTime);
+            stringTime = "";
+
+            int mins = time / 600;
+            int secs = ((time - mins*600) % 600)/10;
+            int oneDP = (time - secs * 10) % 10;
+
+            if (mins > 0)
+            {
+                stringTime += mins + ":";
+                
+                if (secs <= 9)
+                {
+                    stringTime += "0";
+                }
+            }
+
+            stringTime += secs + "." + oneDP;
+
+            return stringTime;
+        }
+        private int GetTimeFromRecord(string stringTime)
+        {
+            string[] splitTime = stringTime.Split('-');
+            return Convert.ToInt32(splitTime[splitTime.Length - 1]);
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            Menu form = new Menu();
+            form.Show();
+            this.Close();
+        }
+    }
+}
