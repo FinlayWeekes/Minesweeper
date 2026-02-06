cb52bb685033114ba618101ea9f87f85ae668552
FinlayWeekes <u241340@crgs.org.uk>
2026-02-06 15:46:47 +0000

    Add files via upload

---
+++ Minesweeper-main/Minesweeper-main/Minesweeper/AI/Difficulty.cs
@@ -0,0 +1,247 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Minesweeper.AI
+{
+    public class Difficulty
+    {
+        public List<Func<LogicCell, Grid, int, int, bool>> EasyPatterns
+        {
+            get
+            {
+                return easyPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> easyPatterns;
+        public List<Func<LogicCell, Grid, int, int, bool>> OptionalPatterns
+        {
+            get
+            {
+                return optionalPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> optionalPatterns;
+        public List<Func<LogicCell, Grid, int, int, bool>> EssentialPatterns
+        {
+            get
+            {
+                return essentialPatterns;
+            }
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> essentialPatterns;
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+        }
+        private int width;
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+        private int height;
+        public int MineCount
+        {
+            get
+            {
+                return mineCount;
+            }
+        }
+        private int mineCount;
+        public string Name
+        {
+            get { return name; }
+        }
+        private string name;
+        public bool Custom
+        {
+            get
+            {
+                return custom;
+            }
+        }
+        private bool custom;
+        public bool Random
+        {
+            get { return random; }
+        }
+        private bool random;
+        public bool MineCounting
+        { 
+            get
+            {
+                return mineCounting;
+            }
+        }
+        private bool mineCounting;
+        public bool MineCountingEssential
+        {
+            get
+            {
+                return mineCountingEssential;
+            }
+        }
+        private bool mineCountingEssential;
+
+        private Dictionary<string, Func<LogicCell, Grid, int, int, bool>> patternsDict = new Dictionary<string, Func<LogicCell, Grid, int, int, bool>>
+        {
+            // Beginner
+            { "B1", BeginnerAI.PB1 },
+            { "B2", BeginnerAI.PB2 },
+
+            // Amateur
+            { "1-1C", AmateurAI.P1_1C },
+            { "2-1", AmateurAI.P2_1 },
+
+            // Intermediate
+            { "1-1CR", IntermediateAI.P1_1CR },
+            { "2-1C", IntermediateAI.P2_1C },
+            { "2-1R", IntermediateAI.P2_1R },
+            { "2-1CR", IntermediateAI.P2_1CR },
+            { "H1", IntermediateAI.PH1 },
+            { "H2", IntermediateAI.PH2 },
+            { "H3", IntermediateAI.PH3 },
+
+            // Expert
+            { "1-1CR+", ExpertAI.P1_1CRx },
+            { "2-1CR+", ExpertAI.P2_1CRx },
+            { "2-1R+", ExpertAI.P2_1Rx },
+            { "H1R", ExpertAI.PH1R },
+            { "H2R", ExpertAI.PH2R },
+            { "H3R", ExpertAI.PH3R },
+            { "1-1T", ExpertAI.P1_1T },
+            { "2-2T", ExpertAI.P2_2T},
+            { "3-2T", ExpertAI.P3_2T },
+
+            // Master
+            { "1-1TR", MasterAI.P1_1TR },
+            { "2-2TR", MasterAI.P2_2TR },
+            { "3-2TR", MasterAI.P3_2TR },
+            { "1-3-1VC", MasterAI.P1_3_1VC },
+            { "1-3-1VCR", MasterAI.P1_3_1VCR },
+            { "2-2-2VC", MasterAI.P2_2_2VC },
+            { "2-2-2VCR", MasterAI.P2_2_2VCR },
+            { "T-Pattern", MasterAI.PT_Pattern}
+        };
+
+        public Difficulty(string difficultyName)
+        {
+            string[] easyNames;
+            string[] optionalNames;
+            string[] essentialNames;
+            mineCounting = false;
+            mineCountingEssential = false;
+
+            using (StreamReader sr = new StreamReader(GetTextFileName(difficultyName)))
+            {
+                sr.ReadLine();
+                this.name = sr.ReadLine();
+
+                sr.ReadLine();
+                this.width = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.height = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.mineCount = Convert.ToInt32(sr.ReadLine());
+
+                sr.ReadLine();
+                this.custom = Convert.ToBoolean(sr.ReadLine());
+
+                sr.ReadLine();
+                easyNames = sr.ReadLine().Split(',');
+
+                sr.ReadLine();
+                optionalNames = sr.ReadLine().Split(',');
+
+                sr.ReadLine();
+                essentialNames = sr.ReadLine().Split(',');
+            }
+
+            this.easyPatterns = GetPatterns(easyNames, false);
+            this.optionalPatterns = GetPatterns(optionalNames, false);
+            this.essentialPatterns = GetPatterns(essentialNames, true);
+
+            CheckForRandom();
+        }
+        public Difficulty(int width, int height, int mineCount, List<string> easyPatternNames, List<string> optionalPatternNames, List<string> essentialPatternNames)
+        {
+            this.mineCounting = false;
+            this.mineCountingEssential = false;
+            this.easyPatterns = GetPatterns(ListToArray(easyPatternNames), false);
+            this.optionalPatterns = GetPatterns(ListToArray(optionalPatternNames), false);
+            this.essentialPatterns = GetPatterns(ListToArray(essentialPatternNames), true);
+            this.width = width;
+            this.height = height;
+            this.mineCount = mineCount;
+            this.name = "Custom";
+            this.custom = true;
+
+            CheckForRandom();
+        }
+
+        private void CheckForRandom()
+        {
+            random = easyPatterns.Count == 0 && optionalPatterns.Count == 0 && essentialPatterns.Count == 0 && !mineCounting; 
+        }
+        private string GetTextFileName(string difficultyName)
+        {
+            return "Difficulties/" + difficultyName.ToLower() + "Difficulty.txt";
+        }
+        private string[] ListToArray(List<string> list)
+        {
+            string[] array = new string[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array [i] = list[i];
+            }
+            return array;
+        }
+        private List<Func<LogicCell, Grid, int, int, bool>> GetPatterns(string[] patternNames, bool essential)
+        {
+            List<Func<LogicCell, Grid, int, int, bool>> patterns = new List<Func<LogicCell, Grid, int, int, bool>>();
+
+            if (patternNames.Length == 0 || patternNames[0] == "null") return patterns;
+
+            foreach (string pattern in patternNames)
+            {
+                if (pattern == "MineCounting")
+                {
+                    mineCounting = true;
+                    mineCountingEssential = essential;
+                }
+                else patterns.Add(patternsDict[pattern]);
+            }
+
+            return patterns;
+        }
+        
+        private List<int> Get3BVValues()
+        {
+
+            using (StreamReader sr = new StreamReader(Get3BVTextFileName()))
+            {
+
+            }
+        }
+        private double CalcStandardDeviation()
+        {
+
+        }
+        private string Get3BVTextFileName()
+        {
+            return "Difficulty Data/3BV/" + name + "3BV.txt"; 
+        }
+    }
+}
---
+++ Minesweeper-main/Minesweeper-main/Minesweeper/AI/Solver.cs
@@ -0,0 +1,182 @@
+using Minesweeper.GUI;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Minesweeper.AI
+{
+    class Solver
+    {
+        private Difficulty difficulty;
+        private Grid grid;
+        public bool HardestPatternFound
+        {
+            get
+            {
+                return hardestPatternFound;
+            }
+        }
+        private bool hardestPatternFound;
+
+        public Solver(Difficulty Difficulty, Grid Grid)
+        {
+            this.difficulty = Difficulty;
+            this.grid = Grid;
+            this.hardestPatternFound = false;
+        }
+
+        public bool IsSolvable()
+        {
+            bool changed;
+            int passes = 0;
+            do
+            {
+                passes++;
+                changed = PassThroughGrid();
+                //System.Diagnostics.Debug.WriteLine("");
+                //grid.DebugDisplayGrid();
+                //System.Diagnostics.Debug.WriteLine("Completed pass: " + passes);
+            }
+            while (changed);
+
+            bool isSolved = grid.IsSolved() && (hardestPatternFound || (difficulty.EssentialPatterns.Count == 0 && !difficulty.MineCountingEssential));
+
+            if (Program.Testing)
+            {
+                if (isSolved)
+                {
+                    WriteDataToFile(Convert.ToString(grid.Find3BV()));
+                }
+                return false;
+            }
+
+            return isSolved;
+        }
+
+        private bool PassThroughGrid()
+        {
+            // checks each cell for easy patterns and if it finds and excecutes one, it will continue looking for easy and wil only check for optional after it finds no easy patterns
+            // it checks for easy patterns (B1 and B2) first as they are both very common and very easy to check compared to some optional patterns which improves efficency
+            // each cell has a boolean value which shows weather it has been completed and completed cells are not checked for patterns
+            // cells are completed when either they are hidden or their effective value is 0 (checked after B2 has been done) 
+            bool changed = false;
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    if (grid.GetCell(x, y).CanBeChecked)
+                    {
+                        if (CheckEasy(x, y))
+                        {
+                            changed = true;
+                        }
+                    }
+                }
+            }
+            if (changed) return true;
+
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    if (grid.GetCell(x, y).CanBeChecked)
+                    {
+                        if (CheckOptional(x, y))
+                        {
+                            changed = true;
+                        }
+                    }
+                }
+            }
+            if (changed) return true;
+
+            //System.Diagnostics.Debug.WriteLine("Checking essential");
+            // essential patterns
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    if (grid.GetCell(x, y).CanBeChecked)
+                    {
+                        if (CheckEssential(x, y))
+                        {
+                            hardestPatternFound = true;
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            // checks minecount at the very end
+            if (difficulty.MineCounting)
+            {
+                if (CheckMineCount())
+                {
+                    hardestPatternFound = difficulty.MineCountingEssential;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // returns true if it finds a pattern on a cell and excecutes it
+        private bool CheckEasy(int x, int y)
+        {
+            foreach (Func<LogicCell, Grid, int, int, bool> easyPattern in difficulty.EasyPatterns)
+            {
+                if (easyPattern(grid.GetCell(x, y), grid, x, y))
+                {
+                    //System.Diagnostics.Debug.WriteLine("Found easy pattern at: " + x + ", " + y);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private bool CheckOptional(int x, int y)
+        {
+            foreach (Func<LogicCell, Grid, int, int, bool> optionalPattern in difficulty.OptionalPatterns)
+            {
+                if (optionalPattern(grid.GetCell(x, y), grid, x, y))
+                {
+                    //System.Diagnostics.Debug.WriteLine("Found optional pattern at: " + x + ", " + y);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private bool CheckEssential(int x, int y)
+        {
+            foreach (Func<LogicCell, Grid, int, int, bool> esentialPatterns in difficulty.EssentialPatterns)
+            {
+                if (esentialPatterns(grid.GetCell(x, y), grid, x, y))
+                {
+                    //System.Diagnostics.Debug.WriteLine("FOUND ESSENTIAL PATTERN AT: " + x + ", " + y);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private bool CheckMineCount()
+        {
+            MineCounting mineCountingSolver = new MineCounting(grid);
+            return mineCountingSolver.Solve();
+        }
+
+        // Methods for board testing
+        private void WriteDataToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(GetTextFileName(), true))
+            {
+                sw.WriteLine(text);
+            }
+        }
+        private string GetTextFileName()
+        {
+            return "Difficulty Data/3BV/" + difficulty.Name + "3BV.txt";
+        }
+    }
+}
